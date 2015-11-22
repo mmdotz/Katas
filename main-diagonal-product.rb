fac7eb7110c79f0ab4576e9bbb0998f7d828916e
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-11-21 19:47:44 -0600

    requires proc for length

--- main-diagonal-product.rb
+++ main-diagonal-product.rb
@@ -1,18 +1,34 @@
 #Given a list of rows of a square matrix, find the product of the main diagonal.
 
 
+#
+# def main_diagonal_product(mat)
+#   n = 0
+#   product =[]
+#   mat.length.times do
+#     product << mat.select{ |x| x[n] }
+#     n+=1
+#     end
+#   product.flatten.inject(:*)
+#   print product
+# end
+#
+# mat = [[1,2,3],[4,5,6],[7,8,9]]
+# main_diagonal_product(mat)
+
+
+#return value at index n+1
 
 def main_diagonal_product(mat)
   n = 0
+  product =[]
   mat.length.times do
-    product = mat.select{ |x| x[n] }
-    [n+=1]
+    product << mat.select{ |x| x[n] }
+    n+=1
     end
-  product = product.inject(:*)
-  puts product
+  product.flatten.inject(:*)
+  print product
 end
 
 mat = [[1,2,3],[4,5,6],[7,8,9]]
 main_diagonal_product(mat)
-
-#inject on nil class
