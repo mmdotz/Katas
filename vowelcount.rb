bbcb09629283941118e5296f17e83a6cc56ab082
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-11-24 15:25:59 -0600

    2nd try failing

--- vowelcount.rb
+++ vowelcount.rb
@@ -1,10 +1,21 @@
 #Return the number (count) of vowels in the given string.
+#
+# def getCount(str)
+#   vowels = ["a", "e", "i", "o", "u"]
+#       print str.chars.count("a, e, i, o, u")
+# end
+#
+# getCount("Michelle")
+#
+# #=> 3
 
+#convert string to array then includes?
+
+#attempt 2
 def getCount(str)
+  string = str.split
   vowels = ["a", "e", "i", "o", "u"]
-      print str.chars.count("a" || "e")
+  print string.include?(vowels)
 end
 
 getCount("Michelle")
-
-#=> 3
