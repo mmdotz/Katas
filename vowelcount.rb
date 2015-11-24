4843fd5820cda751809a356e45639b766a18a4fd
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-11-24 15:05:35 -0600

    failing str.chars.count

--- vowelcount.rb
+++ vowelcount.rb
@@ -1,5 +1,10 @@
 #Return the number (count) of vowels in the given string.
 
 def getCount(str)
-  
+  vowels = ["a", "e", "i", "o", "u"]
+      print str.chars.count("a" || "e")
 end
+
+getCount("Michelle")
+
+#=> 3
