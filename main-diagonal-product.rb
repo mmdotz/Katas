4be7ada7bed128da684d73525c2efc63c43d2ba2
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-11-25 15:58:16 -0600

    push whole array collection into product array-failing

--- main-diagonal-product.rb
+++ main-diagonal-product.rb
@@ -1,34 +1,19 @@
 #Given a list of rows of a square matrix, find the product of the main diagonal.
 
 
-#
-# def main_diagonal_product(mat)
-#   n = 0
-#   product =[]
-#   mat.length.times do
-#     product << mat.select{ |x| x[n] }
-#     n+=1
-#     end
-#   product.flatten.inject(:*)
-#   print product
-# end
-#
-# mat = [[1,2,3],[4,5,6],[7,8,9]]
-# main_diagonal_product(mat)
-
-
 #return value at index n+1
 
-def main_diagonal_product(mat)
+def main_diagonal_product(mat2)
   n = 0
   product =[]
-  mat.length.times do
-    product << mat.select{ |x| x[n] }
+  mat2.length.times do
+    product << mat2.select{ |x| x[n] }
     n+=1
     end
-  product.flatten.inject(:*)
+  product = product.flatten.inject(:*)
   print product
 end
 
+mat2 = [[1,2],[3,4]]
 mat = [[1,2,3],[4,5,6],[7,8,9]]
-main_diagonal_product(mat)
+main_diagonal_product(mat2)
