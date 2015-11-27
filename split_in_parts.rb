6ee667ef5721540a4bc42d77b96956287afc056b
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-11-27 13:23:58 -0600

    trying slice

---
+++ split_in_parts.rb
@@ -0,0 +1,11 @@
+#The aim of this kata is to split a given string into different strings of equal
+ # size (note size of strings is passed to the method)
+
+#  String length is always greater than 0
+# String has no spaces
+# Size is always positive
+
+
+def split_in_parts (s, part_length)
+  s.slice(part_length)
+end
