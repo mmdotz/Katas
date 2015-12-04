ae2462175c1c9f0dfe8eb49f8e140662eb619bcc
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-12-03 19:39:04 -0600

    names shuffled and assigned number

--- yankee_swap.rb
+++ yankee_swap.rb
@@ -1,8 +1,7 @@
 names = ["Brendan", "Ian", "Chloe", "Joanne", "Steve", "Michelle", "Jay", "Mom", "Dad", "Annika", "Charlie"].shuffle
-count_of_names = names.length
 
 n = 1
 names.each do |name|
-  puts n ". #{name}"
+  puts "#{n}. #{name}"
   n+=1
 end
