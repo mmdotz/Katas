2eb7df9b8bda663a3a2c2071d93610f85198339c
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-12-03 19:35:21 -0600

    shuffle names

--- yankee_swap.rb
+++ yankee_swap.rb
@@ -1,2 +1,8 @@
-names = ["Brendan", "Ian", "Chloe", "Joanne", "Steve", "Michelle", "Jay", "Mom", "Dad", "Annika", "Charlie"]
-count_of_names = names.count
+names = ["Brendan", "Ian", "Chloe", "Joanne", "Steve", "Michelle", "Jay", "Mom", "Dad", "Annika", "Charlie"].shuffle
+count_of_names = names.length
+
+n = 1
+names.each do |name|
+  puts n ". #{name}"
+  n+=1
+end
