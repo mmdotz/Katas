7b7bfcd62bb2dfc5d1d6d1a619048f58454183b8
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-12-04 15:02:06 -0600

    solved

--- create_reverse_string_method.rb
+++ create_reverse_string_method.rb
@@ -1,12 +1,12 @@
 def reverse(string)
   string = string.chars  #array
-  @reversed_string = []
+  reversed_string = []
   number_of_chars = string.count
   number_of_chars.times do |chars|
-    @reversed_string << string.pop
-    string
+    reversed_string << string.pop
+    @string = reversed_string.join("")
   end
-  puts @reversed_string
+  puts @string
 end
 
 reverse("Michelle")  #returns reversed string separated by char in array
