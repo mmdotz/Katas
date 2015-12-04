75ee06406843ca5503aa7f5cb63357d854f6f42e
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-12-03 20:52:57 -0600

    puts reversed letter on each line

--- create_reverse_string_method.rb
+++ create_reverse_string_method.rb
@@ -1,14 +1,12 @@
-  def reverse(string)
-    string = string.chars  #array
-    @reversed_string = []
-    number_of_chars = string.count
-    number_of_chars.times do |chars|
-      @reversed_string << string.pop
-      string
-    end
-    @reversed_string.to_s
+def reverse(string)
+  string = string.chars  #array
+  @reversed_string = []
+  number_of_chars = string.count
+  number_of_chars.times do |chars|
+    @reversed_string << string.pop
+    string
   end
-#
-# string = String.create("Michelle")
-# string.reverse
-puts reverse("Michelle")
+  puts @reversed_string
+end
+
+reverse("Michelle")  #returns reversed string separated by char in array
