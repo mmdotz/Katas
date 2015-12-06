5ae464746772c5735641e028cc10dbc390a26c16
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-12-06 13:11:48 -0600

    getting there

--- split_in_parts.rb
+++ split_in_parts.rb
@@ -5,18 +5,23 @@
 # String has no spaces
 # Size is always positive
 
-string = "Michelle"
-part_length = 3
 def split_in_parts (string, part_length)
-  new_string = string[0..part_length-1]+"\s"
-  print string
-  puts new_string
+  n = 0                                            # => 0
+  @new_string =''                                  # => ""
+  number_of_loops = string.length/part_length      # => 4
+  number_of_loops.times do                         # => 4
+    @new_string << string[n..part_length-1]+"\s_"  # => "Mi _", "Mi _ _", "Mi _ _ _", "Mi _ _ _ _"
+    n +=part_length                                # => 2, 4, 6, 8
+  end                                              # => 4
+  puts @new_string                                 # => nil
 end
 
-split_in_parts("Michelle", 3)
+split_in_parts("Michelle", 2)  # => nil
 
 
 
 #tried insert " ", 3 but that only adds it after the first 3 chars, iterating adds a space AFTER the first space created
 #partition doesn't work
 #split and slice don't work
+
+# >> Mi _ _ _ _
