ca835433818e35e5573cde6e81a98395543cbc9f
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-12-08 13:23:03 -0600

    rounds to 2 places only some tests?

---
+++ formatting_decimal_places.rb
@@ -0,0 +1,15 @@
+# Each number should be formatted that it is rounded to two decimal places.
+# No need to check whether the input is a valid number because
+# only valid numbers are used in the tests.
+
+# Example:
+# 5.5589 is rounded 5.56
+# 3.3424 is rounded 3.34
+
+def two_decimal_places(n)
+  puts n.round(2)
+  raise NotImplementedError.new "TODO: two_decimal_places"
+end
+
+
+two_decimal_places(3.56688)
