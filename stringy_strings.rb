06fdc24537b07435b68a9d1ec5c3c89be5b0d6c8
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-12-08 13:08:33 -0600

    solved

--- stringy_strings.rb
+++ stringy_strings.rb
@@ -7,7 +7,7 @@
 # The size will always be positive and will only use whole numbers.
 
 def stringy(size)
-  string = "10" * (size.to_f/2.round)
+  string = "10" * (size.to_f/2).round
   if size.even?
     puts string
   else
