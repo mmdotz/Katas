4b00b9340a68076d1acabcb986649a5dad415f50
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-12-20 10:28:26 -0600

    return -1 for empty array

---
+++ max_diff_length.rb
@@ -0,0 +1,21 @@
+# You are given two arrays a1 and a2 of strings. Each string is composed with letters from a to z.
+# Let x be any string in the first array and y be any string in the second array.
+
+# Find max(abs(length(x) − length(y)))
+
+# If a1 or a2 are empty return -1
+
+a1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
+a2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+# mxdiflg(a1, a2) --> 13
+
+def mxdiflg(a1, a2)
+  if a1.empty? || a2.empty?
+    return -1
+  else
+
+  end
+end
+
+
+mxdiflg(a1, a2)
