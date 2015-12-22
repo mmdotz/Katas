ee525a17e7ed07c1e5cc62e11e8c7abab8cf4c6a
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-12-22 15:32:34 -0600

    complete

--- yankee_swap.rb
+++ yankee_swap.rb
@@ -1,22 +1,26 @@
-names = ["Brendan", "Ian", "Chloe", "Joanne", "Steve", "Michelle", "Jay", "Mom", "Dad", "Annika", "Charlie"].shuffle
 
 puts "Merry Christmas Hall, Dotzenrods and Moniz Families!"
 puts "Please wait a moment while we shuffle the names around...."
 sleep(2)
 
-n = 1
-names.each do |name|
-  puts "#{n}. #{name}"
-  n+=1
+def yank_swap
+  n = 1
+  names = ["Brendan", "Ian", "Chloe", "Joanne", "Steve", "Michelle", "Jay", "Mom", "Dad", "Annika", "Charlie"].shuffle
+  names.each do |name|
+    puts "#{n}. #{name}"
+    n+=1
+  end
+  puts "Are you happy with the results? y/n"
+  answer = gets.chomp
+  if answer.downcase == "y"
+    puts "Thanks have a fun swap!"
+  else
+    yank_swap
+  end
 end
 
-puts "Are you happy with the results? y/n"
-answer = gets.chomp
+yank_swap
 
-if answer.downcase == "y"
-  puts "Thanks have a fun swap!"
-else
-  puts "Press the up arrow to run 'ruby yankee_swap.rb' again."
 
-end
+
 
