f8274a774ebdb0809eb55638def3ff03d12af2aa
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-12-29 15:47:10 -0600

    passing, needed group_by block not using &:itself

--- most_frequent_item_count.rb
+++ most_frequent_item_count.rb
@@ -9,7 +9,7 @@
 # Most frequent number in example array is -1. It occures 5 times in input array.
 
 
-
+#not passing codewars
 def most_frequent_item_count(collection)
   if collection.empty?
     puts "nil"
@@ -19,9 +19,19 @@ def most_frequent_item_count(collection)
   end
 end
 
+#passing, not using itself
+def most_frequent_item_count(collection)
+  if collection.empty?                                                 # => false, true, false, false
+    return 0                                                           # => 0
+  else
+    most_frequent = collection.group_by{|c| c }.values.max_by(&:size)  # => [8], [-3, -3], [0]
+    return most_frequent.count                                         # => 1, 2, 1
+  end
+end
+
 
 most_frequent_item_count([8])                                               # => 1
-most_frequent_item_count([])                                                # => nil
+most_frequent_item_count([])                                                # => 0
 most_frequent_item_count([ -11,-1,1,9,-3,-15,-6,-7,-3,13,4,5,10,13,-9,-7])  # => 2
 most_frequent_item_count([0])                                               # => 1
 # most frequent item is 4, it appears 4 times
