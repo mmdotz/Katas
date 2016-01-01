a2060ddeb32b7edae7db000ef1112561b41f54f1
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-01-01 17:03:48 -0600

    not cubing n, skipping to n-1

--- build_a_pile_of_cubes.rb
+++ build_a_pile_of_cubes.rb
@@ -9,22 +9,24 @@
 # The parameter of the function findNb (find_nb, find-nb) will be an integer m and
 #  you have to return the integer n such as n^3 + (n-1)^3 + ... + 1^3 = m if
 #  such a n exists or -1 if there is no such n.
-
-findNb(1071225) # n = 45
-findNb(91716553919377) # -1
-findNb(90)
-#
+# 
+# findNb(1071225) # n = 45
+# findNb(91716553919377) # -1
+# findNb(90)
+# #
 # notes:
 # if n = 5, then "pile" volume "m" should equal (5)^3 + (5-1)^3 + (4-1)^3 + (3-1)^3 + (2-1)^3
 #   m = 125 + 64 + 27 + 8 + 1
 #   m = 225
-
 def test_find_volume_m(number_of_cubes)
   array_of_cubed_ns = []
   number_of_cubes.times do |n|
     array_of_cubed_ns << n**3
-    n-=
+    number_of_cubes-=1
   end
+  print array_of_cubed_ns
   m = array_of_cubed_ns.inject(:+)
-  print m
+  puts m
 end
+
+test_find_volume_m(5) #=> 225
