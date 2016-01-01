ddc1171818267f770f226e2152ce6322b2c6e8e6
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-01-01 17:06:59 -0600

    test correct using downto

--- build_a_pile_of_cubes.rb
+++ build_a_pile_of_cubes.rb
@@ -9,7 +9,7 @@
 # The parameter of the function findNb (find_nb, find-nb) will be an integer m and
 #  you have to return the integer n such as n^3 + (n-1)^3 + ... + 1^3 = m if
 #  such a n exists or -1 if there is no such n.
-# 
+#
 # findNb(1071225) # n = 45
 # findNb(91716553919377) # -1
 # findNb(90)
@@ -19,14 +19,16 @@
 #   m = 125 + 64 + 27 + 8 + 1
 #   m = 225
 def test_find_volume_m(number_of_cubes)
-  array_of_cubed_ns = []
-  number_of_cubes.times do |n|
-    array_of_cubed_ns << n**3
-    number_of_cubes-=1
-  end
-  print array_of_cubed_ns
-  m = array_of_cubed_ns.inject(:+)
-  puts m
+  array_of_cubed_ns = []                 # => []
+  number_of_cubes.downto(1) do |n|       # => 5
+    array_of_cubed_ns << n**3            # => [125], [125, 64], [125, 64, 27], [125, 64, 27, 8], [125, 64, 27, 8, 1]
+    number_of_cubes-=1                   # => 4, 3, 2, 1, 0
+  end                                    # => 5
+  print array_of_cubed_ns                # => nil
+  m = array_of_cubed_ns.inject(:+)       # => 225
+  puts m                                 # => nil
 end
 
-test_find_volume_m(5) #=> 225
+test_find_volume_m(5)  # => nil
+
+# >> [125, 64, 27, 8, 1]225
