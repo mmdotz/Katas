dd15a9f13befec1d1ef910e48cc5f9c7782394d3
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-01-24 14:06:37 -0600

    passing for less than zero

--- down_arrow_with_numbers.rb
+++ down_arrow_with_numbers.rb
@@ -9,14 +9,24 @@
 #     1
 
 def get_a_down_arrow_of(v)
-  while v >= 1
-    1.upto(v-1) { |x| print x  } && v.downto(1) { |x| print x }
-    puts "\n"
-    v -=1
+  if v < 1
+    return ""
+  else
+    while v >= 1
+      1.upto(v - 1) { |x| print x  } && v.downto(1) { |x| print x }
+      v -=1
+      puts "\n"
+    end
   end
 end
 
+
+
+
+get_a_down_arrow_of(0)
+get_a_down_arrow_of(-7)
 get_a_down_arrow_of(10)
+
 #
 # Test.assert_equals(get_a_down_arrow_of(10), "1234567890987654321\n 12345678987654321\n  123456787654321\n   1234567654321\n    12345654321\n     123454321\n      1234321\n       12321\n        121\n         1");
 #     Test.assert_equals(get_a_down_arrow_of(0), "", "Should be blank for 0");
