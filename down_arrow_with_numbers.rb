d4c33584ee822b13333b48f5f4f7a7f03e20075e
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-01-24 12:54:04 -0600

    printing string to, from n, new line

---
+++ down_arrow_with_numbers.rb
@@ -0,0 +1,20 @@
+# Given a number n, make a down arrow shaped pattern.
+#
+# For example, when n = 5, the output would be:
+#
+# 123454321
+#  1234321
+#   12321
+#    121
+#     1
+
+def get_a_down_arrow_of(v)
+  1.upto(v) { |x| print x } && v.downto(1) { |x| print x }
+  puts "\n"
+end
+
+get_a_down_arrow_of(10)
+#
+# Test.assert_equals(get_a_down_arrow_of(10), "1234567890987654321\n 12345678987654321\n  123456787654321\n   1234567654321\n    12345654321\n     123454321\n      1234321\n       12321\n        121\n         1");
+#     Test.assert_equals(get_a_down_arrow_of(0), "", "Should be blank for 0");
+#     Test.assert_equals(get_a_down_arrow_of(-5), "", "Should be blank for negative numbers");
