41111f5fb8ef1d710ae9bdefad38c9a99f68959e
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-03-13 22:46:14 -0500

    initial commit testing env correct

---
+++ RPS_with_TDD/lib/rockpapersciz.rb
@@ -0,0 +1,11 @@
+#user choice
+#computer choice
+#compare both
+#declare winner based on
+  # rock beats scissors
+  # paper beats rock
+  # scissors beat paper
+
+class Game
+
+end
---
+++ RPS_with_TDD/spec/test_rockpapersciz.rb
@@ -0,0 +1,8 @@
+
+require 'minitest/autorun'
+require 'minitest/pride'
+
+require_relative '../lib/rockpapersciz.rb'
+
+def test_class_Game_exists
+end
