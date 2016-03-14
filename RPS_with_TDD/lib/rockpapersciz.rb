4ebe914faf0b45b55d015cd62a9d7bfbe398f2ef
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-03-13 23:10:24 -0500

    passing output question test

--- RPS_with_TDD/lib/rockpapersciz.rb
+++ RPS_with_TDD/lib/rockpapersciz.rb
@@ -8,4 +8,17 @@
 
 class Game
 
+  choices = ["rock", "paper","scissors"]
+
+  def start_game
+    puts "Would you like to choose rock(1), paper(2), or scissors(3)?"
+    user_choice = gets.chomp
+    puts user_choice
+  end
+
+
+
+
 end
+
+# game = Game.new.start_game
--- RPS_with_TDD/spec/test_rockpapersciz.rb
+++ RPS_with_TDD/spec/test_rockpapersciz.rb
@@ -4,5 +4,19 @@ require 'minitest/pride'
 
 require_relative '../lib/rockpapersciz.rb'
 
-def test_class_Game_exists
+
+class TestGame < Minitest::Unit::TestCase
+
+  def setup
+    @game = Game.new
+  end
+
+  def test_class_Game_exists
+    assert(Game)
+  end
+
+  def test_question_appears
+    assert_output (/like/) {@game.start_game}
+  end
+
 end
