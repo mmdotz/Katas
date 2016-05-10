aaec549fa129f9579c3cc16d28ab469478c01743
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-05-10 11:46:15 -0500

    term does not exceed 4m, is this the goal?

--- Fibonacci.rb
+++ Fibonacci.rb
@@ -15,27 +15,18 @@
 # The 2 is found by adding the two numbers before it (1+1)
 
 
-total = 0
-previous = 0
-i = 1
-
-while i < 400
-  total += i if (i % 2 == 0)
-  i, previous = previous, previous + i
-end
-
-puts total
-
 def generate_Fib_seq_sum
   sum = 0
   prev_term = 0
   term = 1
-  while term < 400
+  sequence = []
+  while term < 4000000
     sum +=term if (term % 2).zero?
-    term = prev_term
-    prev_term = prev_term + term
-  end 
-  p sum
+    term, prev_term = prev_term, prev_term + term
+    sequence << term
+  end
+  print sequence
+  p "The sum of a #{sequence.count} digit Fibonacci series = #{sum}"
 end
 
 generate_Fib_seq_sum
